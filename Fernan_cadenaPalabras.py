b22afbdc3073e5cc5de741fd1199e03629d267dd
USUARIO09-PC\Usuario01 <jekchuris_94@hotmail.com>
2016-11-23 13:39:06 -0500

    Partefinal_Cadenadepalabras

--- Fernan_cadenaPalabras.py
+++ Fernan_cadenaPalabras.py
@@ -1,5 +1,5 @@
 def leertxt():
-    archi=open('poema2.txt','r')
+    archi=open('Jeka.txt','r')
     linea=archi.readline()
     while linea!="":
         print (linea)
@@ -10,3 +10,17 @@ def leertxt():
 leertxt()
 
 
+def contar():
+    archi=open('Jeka.txt','r')
+    archi1=open('resul.txt','w')
+    nump=0
+    for i in archi.readlines():
+        palabras=i.split(" ")
+        nump=nump+len(palabras)
+    print("Numero de palabras que contiene el archivo: ", nump)
+    archi1.write('Numero de palabras que contiene el archivo poema2.txt: '+str(nump))
+    archi.close()
+    archi1.close()
+
+
+contar()
