dd5c1cfd35cff54a10c56db0faf0218c4b9cbb0d
USUARIO09-PC\Usuario01 <jekchuris_94@hotmail.com>
2016-11-23 12:35:58 -0500

    SegundaParteEcuacion

--- Fernan_Ecuacion.py
+++ Fernan_Ecuacion.py
@@ -11,4 +11,32 @@ if(a!=0):
  else:
   x1=(-b+(math.sqrt(disc)))/(2*a)
   x2=(-b-(math.sqrt(disc)))/(2*a)
+else:
+    print("coefiente cuadratico debe ser diferente de cero")
+
+
+def creartxt():
+    archi=open ('Resultado de las raices.txt','w')
+    archi.close()
+
+def grabartxt():
+    archi=open('Resultado de las raices.txt','a')
+    archi.write("X1 = "+str(x1))
+    archi.write("\tX2 = "+str(x2))
+    archi.close()
+    
+def leertxt():
+    archi=open('Resultado de las raices.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print (linea)
+        linea = archi.readline()
+    archi.close
+
+
+
+creartxt()
+grabartxt()
+leertxt()
+
   
