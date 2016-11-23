c6d36b76beb8645b834c2a6f2df52d5fb38b6d10
USUARIO10-PC\Usuario01 <chikifey2122@hotmail.com>
2016-11-23 11:47:28 -0500

    parte3_grabardatos

--- Fernan_CrearArchivo.py
+++ Fernan_CrearArchivo.py
@@ -4,10 +4,23 @@ def creartxt():
 
 def grabartxt():
     archi=open('datos.txt','a')
+    archi.write('Programacion Avanzada\n')
+    archi.write('Aula 16\n')
     archi.write('Fernanada\n')
     archi.write('Jessica\n')
     archi.close()
+    
+def leertxt():
+    archi=open('datos.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print (linea)
+        linea = archi.readline()
+    archi.close
+
 
 
 creartxt()
 grabartxt()
+leertxt()
+
