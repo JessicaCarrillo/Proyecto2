62dccc010843f330e61a30661f33a840f50ac4ff
USUARIO09-PC\Usuario01 <jekchuris_94@hotmail.com>
2016-11-23 11:34:31 -0500

    Parte2_grabar

--- Fernan_CrearArchivo.py
+++ Fernan_CrearArchivo.py
@@ -1,4 +1,13 @@
 def creartxt():
     archi=open ('datos.txt','w')
     archi.close()
-    
+
+def grabartxt():
+    archi=open('datos.txt','a')
+    archi.write('Fernanada\n')
+    archi.write('Jessica\n')
+    archi.close()
+
+
+creartxt()
+grabartxt()
